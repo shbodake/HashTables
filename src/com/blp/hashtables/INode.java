704f90822d92fb289a25719504f85559c9bc2de8
shbodake <sayalibodake28@gmail.com>
2021-11-05 15:49:43 +0530

    Added: find frequency of word in a sentence

---
+++ src/com/blp/hashtables/INode.java
@@ -0,0 +1,21 @@
+package com.blp.hashtables;
+
+public class INode<K> {
+    K getKey() {
+        return null;
+    }
+
+    void setKey(K key) {
+
+    }
+
+    INode getNext() {
+        return null;
+    }
+
+    void setNext(INode next) {
+
+    }
+
+}
+
